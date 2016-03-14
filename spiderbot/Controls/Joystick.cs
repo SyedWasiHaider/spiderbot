1910b91eb0fd7877921b2133045a389de9634a5c
James Clancey <james.clancey@gmail.com>
2016-03-14 01:46:06 -0800

    Moved most the logic for iOS to the shared code

--- spiderbot/Controls/Joystick.cs
+++ spiderbot/Controls/Joystick.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using Xamarin.Forms;
 
 namespace spiderbot
@@ -20,7 +21,10 @@ namespace spiderbot
 
 		public float xPosition {
 			get { return (float)GetValue (xProperty); }
-			set { SetValue (xProperty, value); }
+			set { 
+				SetValue (xProperty, value);
+				this.OnPropertyChanged(); 
+			}
 		}
 
 		public static readonly BindableProperty YProperty =
@@ -28,7 +32,94 @@ namespace spiderbot
 
 		public float yPosition {
 			get { return (float)GetValue (YProperty); }
-			set { SetValue (YProperty, value); }
+			set { 
+				SetValue (YProperty, value);
+				this.OnPropertyChanged();
+			}
+		}
+
+
+		public static readonly BindableProperty ThumbCenterProperty =
+			BindableProperty.Create<Joystick, Point>(p => p.ThumbCenter, Point.Zero);
+
+		public Point ThumbCenter
+		{
+			get { return (Point)GetValue(ThumbCenterProperty); }
+			set { 
+				SetValue(ThumbCenterProperty, value);
+				UpdateValues();
+				this.OnPropertyChanged();
+			}
+		}
+
+		public double Radius { get; set; }
+		protected override void OnSizeAllocated(double width, double height)
+		{
+			base.OnSizeAllocated(width, height);
+			ThumbCenter = new Point(width / 2, height / 2);
+			Radius = Math.Min(width,height)/2;
+		}
+
+		public Point Center => new Point(Width / 2, Height / 2);
+		public bool IsMoving { get; set; }
+
+		public double DistanceFromJoyPad(Point point)
+		{
+			return point.Distance(Center);
+		}
+
+		public bool IsTouchingJoystick(Point point)
+		{
+			var length = DistanceFromJoyPad(point);
+			return length < Radius;
+		}
+		Point touchPos;
+
+		public void MoveJoystick(Point point)
+		{
+			MoveJoystick(point, IsMoving ? Center : ThumbCenter);
+		}
+		public void MoveJoystick(Point point, Point prev)
+		{
+			var offset = point.Subtract(prev);
+
+			//touchPos = touchPos.Add(offset);
+			touchPos = point;
+			var delta = touchPos.Subtract(Center);
+			var newPos = touchPos;
+			var angle = delta.ToAngle();
+			var joybtnDist = DistanceFromJoyPad(newPos);
+			if (joybtnDist > Radius)
+			{
+				var direction = PointHelper.ForAngle(angle);
+				newPos = Center.Add(direction.Multiply(Radius));
+				joybtnDist = Radius;
+			}
+			ThumbCenter = newPos;
+		}
+
+		public void BeingTouch(Point point)
+		{
+			if (IsMoving)
+				return;
+			IsMoving = true;
+			touchPos = Center;
+			MoveJoystick(point);
+
+		}
+
+		public void Stop()
+		{
+			IsMoving = false;
+			ThumbCenter = Center;
+		}
+
+		void UpdateValues()
+		{
+			var distance = ThumbCenter.Subtract(Center).Multiply(1/Radius);
+			xPosition = (float)Math.Round(distance.X, 2);
+			yPosition = (float)Math.Round(distance.Y, 2) * -1;
+			Debug.WriteLine($"X = {xPosition} Y = {yPosition}");
 		}
 	}
 }
